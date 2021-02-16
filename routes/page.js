bade0a2fb56283476d1e9b9833c20a7b09f9943b
DukyoungEom <79042508+DukyoungEom@users.noreply.github.com>
2021-02-17 01:31:07 +0900

    Petition Board updated

--- routes/page.js
+++ routes/page.js
@@ -515,7 +515,7 @@ router.route('/contacts/:pageNum').get(function(req,res){
     
       if(req.query.subcategory && typeof req.query.text  != 'undefined'){
         lost_post = await Post.findAll({
-          attributes:['post_title','category','subcategory','id','number_of_comment'],
+          attributes:['post_title','category','subcategory','id','number_of_comment','like','dislike'],
           where:{
             category:req.query.category,
             subcategory:req.query.subcategory,
@@ -526,7 +526,7 @@ router.route('/contacts/:pageNum').get(function(req,res){
         })
       }else if(req.query.subcategory){
         lost_post = await Post.findAll({
-          attributes:['post_title','category','subcategory','id','number_of_comment'],
+          attributes:['post_title','category','subcategory','id','number_of_comment','like','dislike'],
           where:{
             category:req.query.category,
             subcategory:req.query.subcategory,
@@ -536,7 +536,7 @@ router.route('/contacts/:pageNum').get(function(req,res){
         })
       }else{
         lost_post = await Post.findAll({
-          attributes:['post_title','category','subcategory','id','number_of_comment'],
+          attributes:['post_title','category','subcategory','id','number_of_comment','like','dislike'],
           where:{
             category:req.query.category,
             post_title: {[Op.like]:"%"+req.query.text+"%"}
