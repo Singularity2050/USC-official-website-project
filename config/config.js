b366cbe8cbd1454c69a9a21f71bc64d992a07fe7
Singularity2050 <sije.park@stonybrook.edu>
2021-02-17 22:30:25 +0900

    Club list
    Club list

--- config/config.js
+++ config/config.js
@@ -3,19 +3,19 @@ require('dotenv').config();
 module.exports = {
   
     development: {
-      username: "root",
-      password:process.env.SEQUELUZE_PASSWORD,
-      database: "usc",
-      host: "127.0.0.1",
-      dialect: "mysql",
+      username: "sunykorea",
+    password: "soonwechat36",
+    database: "usc",
+    host: "database-1.csicbi1sqrpx.ap-northeast-2.rds.amazonaws.com",
+    dialect: "mysql"
       
     },
     production: {
-      username: "root",
-      password: process.env.SEQUELUZE_PASSWORD,
-      database: "usc",
-      host: "127.0.0.1",
-      dialect: "mysql",
+      username: "sunykorea",
+    password: "soonwechat36",
+    database: "usc",
+    host: "database-1.csicbi1sqrpx.ap-northeast-2.rds.amazonaws.com",
+    dialect: "mysql"
       
       
     },
